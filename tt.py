8f1cc492f26ac183074fc4f4fb1fa1ef6deb5895
sinjupayyeri <44390335+sinjupayyeri@users.noreply.github.com>
2021-02-13 09:56:52 +0530

    new lines added

--- tt.py
+++ tt.py
@@ -4,3 +4,11 @@ for m in List1:
     if m>n:
     n=m
 print(m," is the largest element in the list")
+
+List1=[12,2,34,33,67,54]
+n=List1[0]
+for m in List1:
+    if m>n:
+    n=m
+print(m," is the largest element in the list")
+
